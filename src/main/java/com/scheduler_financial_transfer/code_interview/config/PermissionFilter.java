84f01c598f8fb15ff50941551bedcd8bf2ae9c61
Vitor Cosso <vitorcosso@gmail.com>
2025-08-27 20:03:08 -0300

    feat: implemeting simple scheduler

--- src/main/java/com/scheduler_financial_transfer/code_interview/config/PermissionFilter.java
+++ src/main/java/com/scheduler_financial_transfer/code_interview/config/PermissionFilter.java
@@ -3,6 +3,9 @@ package com.scheduler_financial_transfer.code_interview.config;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class PermissionFilter extends BasicAuthenticationFilter {
@@ -22,7 +26,14 @@ public class PermissionFilter extends BasicAuthenticationFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        System.out.println("LOGADO COM SUCESSO");
+
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(
+                        request.getHeader("login"),
+                        null,
+                        Collections.singleton(new SimpleGrantedAuthority("ROLE_" + request.getHeader("x-permission-x")))
+                )
+        );
         chain.doFilter(request,response);
     }
 }
